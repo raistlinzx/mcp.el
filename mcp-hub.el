b6b66089b970dd2966979e6114c924a150012528
Kien Nguyen <kien.n.quang@gmail.com>
2025-05-14 13:41:31 -0700

    mcp-hub-start-all-server: check servers before remove from to-start list

--- mcp-hub.el
+++ mcp-hub.el
@@ -121,8 +121,9 @@ Optional argument SERVERS is a list of server names (strings) to filter which
 servers should be started. When nil, all configured servers are considered."
   (interactive)
   (let* ((servers-to-start (cl-remove-if (lambda (server)
-                                           (or (not (cl-find (car server) servers :test #'string=))
-                                               (gethash (car server) mcp-server-connections)))
+                                           (if servers
+                                               (not (cl-find (car server) servers :test #'string=))
+                                             (gethash (car server) mcp-server-connections)))
                                          mcp-hub-servers))
          (total (length servers-to-start))
          (started 0))
